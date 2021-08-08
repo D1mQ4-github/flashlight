3e97a0f09079c59277a4b28a04d850e4c7bfd13e
D1mQ4-github <nounejmbatkovic@gmail.com>
2021-08-08 13:44:54 +0300

    scoreboard and game object added

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let flashlightSize = 150;
+    let flashlightSize = 150,
+        score = 0;
 
     const $flashlight = document.querySelector('.dynamic-image'),
         $wrapper = document.querySelector('.wrapper'),
-        $shadow = document.querySelector('.shadow');
+        $shadow = document.querySelector('.shadow'),
+        $score = document.querySelector('.score'),
+        $object = document.querySelector('.object');
+
+    $score.textContent = score;
+
+    const getMoney = (e) => {
+        const amount = e.target.parentElement.dataset.amount;
+        score += +amount;
+        $score.textContent = score;
+    }
+
+    const getRandomAmount = (max) => {
+        const num = Math.floor(Math.random() * max);
+        return ((num) < 1) ? getRandomAmount(max) : num;
+    }
+
+    $object.addEventListener('click', getMoney);
 
     $wrapper.addEventListener('mousemove', (e) => {
         let x = e.clientX,
